e8b8f2dc9d41490e85bb04aeb6c54e77eefdaf3b
Pat <prdepuydt@gmail.com>
2013-07-02 08:10:35 -0400

    Checkpoint
    .

---
+++ 7-2-13_Hoverzoom_v4/js/hoverZoomThumbWidths.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+				$('#etalage').etalage({
+					thumb_image_width: 300,
+					thumb_image_height: 400,
+					source_image_width: 900,
+					source_image_height: 1200
+				});
+			});
